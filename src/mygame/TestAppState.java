4be8c40c7067991e78bf4be077d3c6a8b041b9bf
Domenic <domenic.cassisi@web.de>
2017-04-02 18:14:02 +0200

    Initial Commit

---
+++ src/mygame/TestAppState.java
@@ -0,0 +1,19 @@
+package mygame;
+
+import com.jme3.app.state.AbstractAppState;
+
+/**
+ * Created by Domenic on 31.03.2017.
+ */
+public class TestAppState extends AbstractAppState {
+
+  private int x = 100;
+    
+    public void setX(int x) {}
+
+
+
+  public int getX() {
+    return x;
+  }
+}
