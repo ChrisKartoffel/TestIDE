005d45cd25a82601bdb6134eb65b1be5f41db034
Domenic <domenic.cassisi@web.de>
2017-04-02 18:22:25 +0200

    tesr3

--- src/mygame/TestAppState.java
+++ src/mygame/TestAppState.java
@@ -6,7 +6,7 @@ import com.jme3.app.state.AbstractAppState;
  * Created by Domenic on 31.03.2017.
  */
 public class TestAppState extends AbstractAppState {
-    
+
     private int x = 100;
 
     public void setX(int x) {}
