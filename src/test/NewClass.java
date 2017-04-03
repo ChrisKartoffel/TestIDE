c0321ca4b9ff643a74e7e66c16005027f1ec5d43
Domenic <domenic.cassisi@web.de>
2017-04-03 23:09:12 +0200

    Commit of Main

--- src/test/NewClass.java
+++ src/test/NewClass.java
@@ -1,11 +1,11 @@
 package test;
 
 /**
+ * Doc
  * Created by Domenic on 02.04.2017.
  */
 public class NewClass {
 
-    // TEST CLASS !!! EDITED HERE TEST
 
     //    HIER GIBT ES NICHTS ZU SEHEN GEHEN SIE WEITER!!;
 
