db3f5e1c39c60006774c5a83e175d44c7bb40ec6
Domenic <domenic.cassisi@web.de>
2017-04-04 15:25:10 +0200

    Commit of Main

--- src/test/Main.java
+++ src/test/Main.java
@@ -27,7 +27,9 @@ public class Main extends SimpleApplication {
     public void simpleInitApp() {
         Box b = new Box(1, 1, 1);
         Geometry geom = new Geometry("Box", b);
-        geom.setLocalTranslation(1, 1, 1);
+        geom.setLocalTranslation(1, 0, 1);
+
+        // added comment
 
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Blue);
